0758f1bd11ba3381080f94df4e4810aa829b9cb9
Oskars24 <oskarsycz@gmail.com>
2019-09-17 10:50:37 +0200

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/oskars-craft-beer/'
-    : '/'
-}
+    publicPath: process.env.NODE_ENV === 'production' ? '/oskars-craft-beer/' : '/'
+  }
