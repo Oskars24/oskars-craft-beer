71b2ffc6499652c35900c903fa37f62a9263360b
Oskars24 <oskarsycz@gmail.com>
2019-09-26 16:57:10 +0200

    Add receptury content

--- src/components/receptury.vue
+++ src/components/receptury.vue
@@ -1,30 +1,88 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-  </div>
+    <div class="recepturyPage">
+        <div class="articleTitle">WYBIERZ RECEPTURĘ</div>
+        <div class="beerLabel" v-for="beer in beerList" :key="beer.id">
+            <div class="beerImg"><img :src="labelSrc(beer.id)"></div>
+            <div class="beerTitle">{{ beer.title }}</div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'receptury',
-  data () {
-    return {
-      msg: 'Witaj w warki.vue'
+    name: 'receptury',
+    data () {
+        return {
+            beerList: this.sortBeer(require("@/content/list.json")),
+        }
+    },
+    methods: {
+        sortBeer: function(array) {
+            if (array[0].id != 1) {
+                array.sort(function(a,b){return a[1] - b[1]})
+            }
+            return array.reverse()
+        },
+        labelSrc: function(number) {
+            return require("@/content/labels/label_"+number+".jpg")
+        },
     }
-  },
-
-  }
+}
 
 </script>
 
 <style>
-    .beerNav > .router-link-exact-active:nth-child(3) ~ .glass {
-    transform: translateY(7px) translateX(calc((var(--globalWidth) - 200px) / 12*-1 - 100px)) perspective(60px) rotatex(-15deg);
-    }
+.beerNav > .router-link-exact-active:nth-child(3) ~ .glass {
+transform: translateY(7px) translateX(calc((var(--globalWidth) - 200px) / 12*-1 - 100px)) perspective(60px) rotatex(-15deg);
+}
 </style>
 
 <style scoped>
-    h1 {
-      color: gold;
-    }
+.recepturyPage {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    justify-items: center;
+}
+
+.beerLabel {
+    width: 140px;
+    margin: 5px 5px 0 5px;
+    text-align: center;
+    background-color: white;
+    border: solid 2px transparent;
+    padding: 20px 10px 10px 10px;
+}
+
+.beerLabel:hover {
+    background-color: #FAA432;
+    border: solid 2px #fff152;
+}
+
+.beerLabel:hover > .beerImg {
+    transform: scale(1.05);
+    transform-origin: bottom;
+}
+
+.beerLabel:active > .beerTitle {
+    color: #fff152;
+}
+
+.beerImg {
+    transition: transform 0.3s ease-in-out;
+}
+
+.beerImg > img {
+    width: 100%;
+}
+
+.beerTitle {
+    font-weight: bold;
+    padding: 0 10px 10px 10px;
+}
+
+.articleTitle {
+    font-size: 25px;
+    grid-column: 1/-1;
+    text-align: center;
+}
 </style>
